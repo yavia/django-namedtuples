b2380091c107c5ec113ef11ec7c9db4a96491760
Konstantin Gukov <gukoff@yandex-team.ru>
2018-04-02 20:10:19 +0500

    1.0.0 release

--- setup.py
+++ setup.py
@@ -20,9 +20,9 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.2',
-    url='https://github.com/gukoff/django-namedtuples',
-    download_url='https://github.com/gukoff/django-namedtuples/archive/0.0.2.tar.gz',
+    version='1.0.0',
+    url='https://github.com/yavia/django-namedtuples',
+    download_url='https://github.com/yavia/django-namedtuples/archive/1.0.0.tar.gz',
 
     license='GPLv3',
 
